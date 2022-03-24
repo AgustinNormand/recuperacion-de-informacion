7a8aa2229baecfb1cabf15de0e545ae186f1fec8
Agustin Normand <NormandAgustin@gmail.com>
2022-03-23 23:31:57 -0300

    Avanzo con el ejercicio 4

--- TP_01/ejercicio_4/tokenizer_simplified.py
+++ TP_01/ejercicio_4/tokenizer_simplified.py
@@ -1,6 +1,8 @@
 import pathlib
 import sys
 import re
+from nltk.stem import *
+
 
 class Tokenizer:
 	def __init__(self, dirpath, empty_words_path):
@@ -9,7 +11,32 @@ class Tokenizer:
 
 		self.load_empty_words(empty_words_path)
 
+		self.lancasterStemmer = LancasterStemmer()
+		self.porterStemmer = PorterStemmer()
+
+		self.unique_words = []
+
+		#self.totalWords = 0
+		self.sameTreatedWords = 0
+		self.differentTreatedWords = 0
+
+		self.termsLancaster = []
+		self.termsPorter = []
+
 		self.process_collection()
+
+		self.stem_unique_words()
+
+		#print(len(self.unique_words))
+
+	#	print("Cantidad tokens Lancaster:{}, Cantidad tokens Porter:{}".format(
+	#		len(self.termsLancaster), len(self.termsPorter)))
+		#print("TotalWords:{}, SameTreatedWords:{}, DifferentTreatedWords:{}".format(
+		#	self.totalWords, self.sameTreatedWords, self.differentTreatedWords))
+
+		#print("set(self.termsLancaster) - set(self.termsPorter)):{}".format(list(set(self.termsLancaster) - set(self.termsPorter))))
+
+		#print("set(self.termsPorter) - set(self.termsLancaster)):{}".format(list(set(self.termsPorter) - set(self.termsLancaster))))
 
 	def load_empty_words(self, empty_words_path):
 		if empty_words_path:
@@ -21,7 +48,30 @@ class Tokenizer:
 		for in_file in self.corpus_path.iterdir():
 			with open(in_file, "r") as f:
 				for line in f.readlines():
-					tokens_list =  filter(lambda x: x not in self.palabras_vacias, [self.process(x) for x in line.strip().split()])
+					for word in line.strip().split():
+						word = self.normalize(word)
+						try:
+							int(word)
+						except:
+							if word not in self.unique_words:
+								self.unique_words.append(word)
+
+	def stem_unique_words(self):
+		for word in self.unique_words:
+			lancasterStem = self.lancasterStemmer.stem(word)
+			porterStem = self.porterStemmer.stem(word)
+			if lancasterStem not in self.termsLancaster:
+				self.termsLancaster.append(lancasterStem)
+
+			if porterStem not in self.termsPorter:
+				self.termsPorter.append(porterStem)
+
+			if lancasterStem != porterStem:
+				print("Word:{}, PorterStem:{}, LancasterStem:{}".format(word, porterStem, lancasterStem))
+				self.differentTreatedWords += 1
+			else:
+				#print("Word:{}, PorterStem:{}, LancasterStem:{}".format(word, porterStem, lancasterStem))
+				self.sameTreatedWords += 1
 
 	def translate(self, to_translate):
 		tabin = u'áéíóú'
@@ -40,12 +90,14 @@ class Tokenizer:
 		return result
 
 	def process(self, token):
-		result = self.normalize(token)
+		result = token
+		#result = self.normalize(result)
 		result = self.stem(result)
 		return result
-	
-	def stem(self, token):
-		
+
+	# def stem(self, token):
+	#	return self.stemmer.stem(token)
+
 
 if __name__ == '__main__':
 	if len(sys.argv) < 2:
