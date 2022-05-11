23a2aeb65e8f73f45b3c2e1a275dede7114afe8a
Agustin Normand <normandagustin@gmail.com>
2022-05-11 13:24:56 -0300

    Ploteo resultados

--- TP_04/ejercicio_4/script_1/plot_results.py
+++ TP_04/ejercicio_4/script_1/plot_results.py
@@ -1,10 +1,10 @@
 times = {}
-times[76] = [276.0338673591614, 46.835002183914185]
-times[151] = [327.7100336551666, 38.12986445426941]
-times[302] = [387.84252977371216, 23.397321462631226]
-times[604] = [325.9673557281494, 22.63266921043396]
-times[1210] = [397.2900092601776, 15.86511754989624]
-times[2420] = [363.08713603019714, 14.368023872375488]
+times[76] = [271.8071989218394, 22.21466628710429]
+times[151] = [272.64158058166504, 18.06307601928711]
+times[302] = [271.07223471005756, 15.516569375991821]
+times[604] = [274.42263730367023, 13.907117366790771]
+times[1210] = [273.58787059783936, 12.69956644376119]
+times[2420] = [275.2257897059123, 12.186079740524292]
 
 import matplotlib.pyplot as plt
 
@@ -16,11 +16,16 @@ for key in times:
 
 plt.figure(0)
 plt.plot(times.keys(), index_times)
-plt.show()
+plt.xlabel("Limite de documentos")
+plt.ylabel("Tiempo de indexación")
+plt.savefig("./output/index")
 
 plt.figure(1)
 plt.plot(times.keys(), merge_times)
-plt.show()
-#plt.xlabel("Overhead")
-#plt.ylabel("Cantidad de documentos")
+plt.xlabel("Limite de documentos")
+plt.ylabel("Tiempo de merge")
+plt.savefig("./output/merge")
+
+print("Index range: {} {}".format(min(index_times), max(index_times)))
+print("Merge range: {} {}".format(min(merge_times), max(merge_times)))
 
